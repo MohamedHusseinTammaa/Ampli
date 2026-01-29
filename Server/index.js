8ea4705129f9ac130d686bcda913dd8c0a1f1fd9
Mohamed Hussein <muhamed.tammaa@gmail.com>
2026-01-29 04:03:47 +0200

    added auth (closes #1 , #2)

--- Server/index.js
+++ Server/index.js
@@ -3,9 +3,10 @@ import {router as AiRouter } from './Routers/Ai.router.js';
 import "dotenv/config"
 import Express from 'express';
 import cors from "cors";
+import connectDB from './Database/DbConnection.js';
 const app = Express();
 const PORT = process.env.PORT || 3000;
-
+connectDB();
 app.use(
   cors({
     origin: '*', 
@@ -19,8 +20,21 @@ app.use(express.json({ limit: '11mb' }));
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok' });
 });
-app.use('api/ai',AiRouter)
 
+app.use('api/User',UsersRouter);
+app.use('api/ai',AiRouter);
+app.use((req,res)=>{
+  res.status(404).json({
+    message: 'Route not found',
+    status: 'NotFound',
+  });
+});
+app.use((err,req,res,next)=>{
+  res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error',
+    status:err.status||'error',
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
